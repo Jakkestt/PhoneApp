2752ae4812b7b18c3309107d59f6f5998d42efc7
Jarkko Arffman <41551796+Jakkestt@users.noreply.github.com>
2024-02-04 19:47:56 +0200

    Homework 3 done

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
+    id("com.google.devtools.ksp")
 }
 
 android {
@@ -47,6 +48,32 @@ android {
 }
 
 dependencies {
+    val room_version = "2.5.0"
+
+    implementation("androidx.room:room-runtime:$room_version")
+    annotationProcessor("androidx.room:room-compiler:$room_version")
+
+    // To use Kotlin Symbol Processing (KSP)
+    ksp("androidx.room:room-compiler:$room_version")
+
+    // optional - Kotlin Extensions and Coroutines support for Room
+    implementation("androidx.room:room-ktx:$room_version")
+
+    // optional - RxJava2 support for Room
+    implementation("androidx.room:room-rxjava2:$room_version")
+
+    // optional - RxJava3 support for Room
+    implementation("androidx.room:room-rxjava3:$room_version")
+
+    // optional - Guava support for Room, including Optional and ListenableFuture
+    implementation("androidx.room:room-guava:$room_version")
+
+    // optional - Test helpers
+    testImplementation("androidx.room:room-testing:$room_version")
+
+    // optional - Paging 3 Integration
+    implementation("androidx.room:room-paging:$room_version")
+
     val nav_version = "2.7.6"
 
     // Kotlin
@@ -62,9 +89,11 @@ dependencies {
     // Jetpack Compose Integration
     implementation("androidx.navigation:navigation-compose:$nav_version")
 
-    implementation("androidx.core:core-ktx:1.10.1")
-    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.1")
-    implementation("androidx.activity:activity-compose:1.7.0")
+    implementation("androidx.fragment:fragment-ktx:1.6.2")
+    implementation("androidx.activity:activity-ktx:1.8.2")
+    implementation("androidx.core:core-ktx:1.12.0")
+    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.7.0")
+    implementation("androidx.activity:activity-compose:1.8.2")
     implementation(platform("androidx.compose:compose-bom:2023.08.00"))
     implementation("androidx.compose.ui:ui")
     implementation("androidx.compose.ui:ui-graphics")
--- app/src/main/java/com/example/phoneapp/MainActivity.kt
+++ app/src/main/java/com/example/phoneapp/MainActivity.kt
@@ -1,47 +1,106 @@
 package com.example.phoneapp
 
+import android.content.Context
 import android.content.res.Configuration
+import android.net.Uri
 import android.os.Bundle
 import androidx.activity.ComponentActivity
+import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.compose.setContent
+import androidx.activity.result.PickVisualMediaRequest
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.compose.animation.animateColorAsState
 import androidx.compose.animation.animateContentSize
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.core.net.toUri
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import androidx.room.ColumnInfo
+import androidx.room.Dao
+import androidx.room.Database
+import androidx.room.Delete
+import androidx.room.Entity
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.PrimaryKey
+import androidx.room.Query
+import androidx.room.Room
+import androidx.room.RoomDatabase
+import androidx.room.Update
 import coil.compose.AsyncImage
+import com.example.phoneapp.SampleData
 import com.example.phoneapp.ui.theme.PhoneAppTheme
-import com.example.phoneapp.SampleData as SampleData
+import java.io.File
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        val db = Room.databaseBuilder(
+            applicationContext,
+            AppDatabase::class.java, "database-name"
+        ).allowMainThreadQueries().build()
+
         setContent {
-            App()
+            App(database = db)
         }
     }
+}
+
+@Entity
+data class User(
+    @PrimaryKey val uid: Int,
+    @ColumnInfo(name = "profile_name", defaultValue = "Test Name") val profileName: String,
+    @ColumnInfo(name = "selected_image") val selectedImage: String,
+)
+
+@Dao
+interface UserDao {
+    @Query("SELECT * FROM user")
+    fun getAll(): List<User>
+
+    @Insert
+    fun insertUser(user: User)
+
+    @Update(onConflict = OnConflictStrategy.REPLACE)
+    fun updateUser(user: User)
+
+    @Delete
+    fun deleteUser(user: User)
+}
+
+@Database(entities = [User::class], version = 1)
+abstract class AppDatabase : RoomDatabase() {
+    abstract fun userDao(): UserDao
 }
 
 @Composable
@@ -49,7 +108,61 @@ fun App(
     modifier: Modifier = Modifier,
     navController: NavHostController = rememberNavController(),
     startDestination: String = "Chat",
+    database: AppDatabase,
 ) {
+    val userDao = database.userDao()
+    val size: List<User> = userDao.getAll()
+    if (size.isEmpty()) {
+        val user = User(
+            uid = 1,
+            profileName = "Test Name",
+            selectedImage = Uri.EMPTY.toString()
+        )
+        userDao.insertUser(user)
+    }
+    val users: List<User> = userDao.getAll()
+    var selectedImage by remember {
+        mutableStateOf<Uri>(users[0].selectedImage.toUri())
+    }
+
+    var profileName by remember {
+        mutableStateOf<String>(users[0].profileName)
+    }
+
+    val context = LocalContext.current
+
+    val pickMedia = rememberLauncherForActivityResult(
+        contract = ActivityResultContracts.PickVisualMedia(),
+        onResult = { uri ->
+            if (uri != null) {
+                val new_uri = saveImageToInternalStorage(context, uri)
+                selectedImage = new_uri
+                val user = User(
+                    uid = users[0].uid,
+                    profileName,
+                    selectedImage.toString()
+                )
+                userDao.updateUser(user)
+            }
+        },
+    )
+
+    fun changeName(it: String) {
+        profileName = it
+        val user = User(
+            uid = users[0].uid,
+            profileName,
+            selectedImage.toString()
+        )
+        userDao.updateUser(user)
+    }
+
+    fun launchPhotoPicker() {
+        pickMedia.launch(
+            PickVisualMediaRequest(ActivityResultContracts.PickVisualMedia.ImageOnly)
+        )
+    }
+
     NavHost(
         modifier = modifier,
         navController = navController,
@@ -58,12 +171,22 @@ fun App(
         composable("Chat") {
             PhoneAppTheme {
                 MainScreen(
-                    onNavigateToProfile = { navController.navigate("Profile") }
+                    onNavigateToProfile = {
+                        navController.navigate("Profile")
+                    },
+                    selectedImage = selectedImage,
+                    profileName = profileName
                 )
             }
         }
         composable("Profile") {
-            ProfileScreen(onNavigateBack = { navController.popBackStack() })
+            ProfileScreen(
+                onNavigateBack = { navController.popBackStack() },
+                onPickImage = { launchPhotoPicker() },
+                selectedImage = selectedImage,
+                profileName = profileName,
+                changeName = { changeName(it) },
+            )
         }
     }
 }
@@ -71,35 +194,91 @@ fun App(
 @Composable
 fun MainScreen(
     onNavigateToProfile: () -> Unit,
+    selectedImage: Uri,
+    profileName: String,
 ) {
     Column {
         Button(onClick = onNavigateToProfile) {
             Text(text = "Profile")
         }
-        Conversation(messages = SampleData.conversationSample)
+        Conversation(
+            messages = SampleData.conversationSample,
+            selectedImage = selectedImage,
+            profileName = profileName
+        )
     }
 }
 
 @Composable
 fun ProfileScreen(
     onNavigateBack: () -> Unit,
+    onPickImage: () -> Unit,
+    selectedImage: Uri,
+    profileName: String,
+    changeName: (String) -> Unit
 ) {
     Column {
         Button(onClick = onNavigateBack) {
             Text(text = "Back")
         }
+        Button(onClick = { onPickImage() }) {
+            Text(text = "Choose Image")
+        }
+        if (selectedImage != Uri.EMPTY) {
+            AsyncImage(
+                model = selectedImage,
+                contentDescription = "Image",
+                modifier = Modifier
+                    .size(40.dp)
+                    .clip(CircleShape)
+                    .border(1.5.dp, MaterialTheme.colorScheme.primary, CircleShape)
+            )
+        } else {
+            Image(
+                painter = painterResource(id = R.drawable._50px_hl_gonarch_model),
+                contentDescription = "Image",
+                modifier = Modifier
+                    .size(40.dp)
+                    .clip(CircleShape)
+                    .border(1.5.dp, MaterialTheme.colorScheme.primary, CircleShape)
+            )
+        }
+        TextField(
+            value = profileName,
+            onValueChange = { changeName(it) },
+            label = { Text("Profile Name") }
+        )
     }
 }
 
 data class Message(val author: String, val body: String)
 
 @Composable
-fun Function(msg: Message) {
+fun Function(
+    msg: Message,
+    selectedImage: Uri,
+    profileName: String
+) {
     Row(modifier = Modifier.padding(8.dp)) {
-        AsyncImage(
-            model = "https://jakkest.xyz/images/xmrqrcode.png",
-            contentDescription = "Image"
-        )
+        if (selectedImage != Uri.EMPTY) {
+            AsyncImage(
+                model = selectedImage,
+                contentDescription = "This is an example image",
+                modifier = Modifier
+                    .size(40.dp)
+                    .clip(CircleShape)
+                    .border(1.5.dp, MaterialTheme.colorScheme.primary, CircleShape)
+            )
+        } else {
+            Image(painter = painterResource(
+                id = R.drawable._50px_hl_gonarch_model),
+                contentDescription = "Image",
+                modifier = Modifier
+                    .size(40.dp)
+                    .clip(CircleShape)
+                    .border(1.5.dp, MaterialTheme.colorScheme.primary, CircleShape)
+            )
+        }
 
         Spacer(modifier = Modifier.width(8.dp))
 
@@ -111,7 +290,7 @@ fun Function(msg: Message) {
 
         Column(modifier = Modifier.clickable { isExpanded = !isExpanded }) {
             Text(
-                text = msg.author,
+                text = profileName,
                 color = MaterialTheme.colorScheme.secondary,
                 style = MaterialTheme.typography.titleSmall,
             )
@@ -147,16 +326,27 @@ fun Function(msg: Message) {
 fun PreviewFunction() {
     PhoneAppTheme {
         Surface {
-            Function(Message("Jarkko", "Hello"))
+            Function(Message(
+                "Jarkko",
+                "Hello"
+            ), profileName = "Jarkko", selectedImage = Uri.EMPTY)
         }
     }
 }
 
 @Composable
-fun Conversation(messages: List<Message>) {
+fun Conversation(
+    messages: List<Message>,
+    selectedImage: Uri,
+    profileName: String
+) {
     LazyColumn {
         items(messages) { message ->
-            Function(message)
+            Function(
+                message,
+                selectedImage = selectedImage,
+                profileName = profileName
+            )
         }
     }
 }
@@ -165,6 +355,22 @@ fun Conversation(messages: List<Message>) {
 @Composable
 fun PreviewConversation() {
     PhoneAppTheme {
-        Conversation(SampleData.conversationSample)
+        Conversation(
+            SampleData.conversationSample,
+            profileName = "Jarkko",
+            selectedImage = Uri.EMPTY
+        )
     }
+}
+
+fun saveImageToInternalStorage(context: Context, uri: Uri): Uri {
+    val inputStream = context.contentResolver.openInputStream(uri)
+    val outputStream = context.openFileOutput("image.jpg", Context.MODE_PRIVATE)
+    inputStream?.use { input ->
+        outputStream.use { output ->
+            input.copyTo(output)
+        }
+    }
+    val file = File(context.filesDir, "image.jpg")
+    return Uri.fromFile(file)
 }
